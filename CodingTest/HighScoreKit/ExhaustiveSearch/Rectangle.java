106e1d0d1d57d2992950c4998f38e76e3d7f474b
Jiseoup <jseoup@gmail.com>
2025-04-26 02:27:22 +0900

    Solve Problems

---
+++ CodingTest/HighScoreKit/ExhaustiveSearch/Rectangle.java
@@ -0,0 +1,56 @@
+// [Lv.1] 최소 직사각형
+
+package CodingTest.HighScoreKit.ExhaustiveSearch;
+
+import java.util.*;
+
+public class Rectangle {
+    public static void main(String[] args) {
+        // Examples
+        List<TestCase> testCases = List.of(
+            new TestCase(new int[][] {{60, 50}, {30, 70}, {60, 30}, {80, 40}}),
+            new TestCase(new int[][] {{10, 7}, {12, 3}, {8, 15}, {14, 7}, {5, 15}}),
+            new TestCase(new int[][] {{14, 4}, {19, 6}, {6, 16}, {18, 7}, {7, 11}})
+        );
+
+        // Solutions
+        Solution sol = new Solution();
+        for (TestCase t : testCases) {
+            int result = sol.solution(t.sizes);
+            System.out.println(result);
+        }
+    }
+}
+
+
+// Create TestCase
+class TestCase {
+    int[][] sizes;
+
+    TestCase(int[][] sizes) {
+        this.sizes = sizes;
+    }
+}
+
+
+class Solution {
+    public int solution(int[][] sizes) {
+        int maxWidth = 0;
+        int maxHeight = 0;
+
+        for (int[] size : sizes) {
+            int width = size[0];
+            int height = size[1];
+            
+            if (width > height) {
+                maxWidth = Math.max(maxWidth, width);
+                maxHeight = Math.max(maxHeight, height);
+            }
+            else {
+                maxWidth = Math.max(maxWidth, height);
+                maxHeight = Math.max(maxHeight, width);
+            }
+        }
+        return maxWidth * maxHeight;
+    }
+}
---
+++ CodingTest/KakaoBlindRecruitment2023/PersonalInformation.java
@@ -0,0 +1,91 @@
+// [Lv.1] 완주하지 못한 선수
+
+package CodingTest.KakaoBlindRecruitment2023;
+
+import java.util.*;
+
+public class PersonalInformation {
+    public static void main(String[] args) {
+        // Examples
+        List<TestCase> testCases = List.of(
+            new TestCase(
+                "2022.05.19",
+                new String[] {"A 6", "B 12", "C 3"},
+                new String[] {"2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"}
+            ),
+            new TestCase(
+                "2020.01.01",
+                new String[] {"Z 3", "D 5"},
+                new String[] {"2019.01.01 D", "2019.11.15 Z", "2019.08.02 D", "2019.07.01 D", "2018.12.28 Z"}
+            )
+        );
+
+        // Solutions
+        Solution sol = new Solution();
+        for (TestCase t : testCases) {
+            int[] result = sol.solution(t.today, t.terms, t.privacies);
+            System.out.println(Arrays.toString(result));
+        }
+    }
+}
+
+
+// Create TestCase
+class TestCase {
+    String today;
+    String[] terms;
+    String[] privacies;
+
+    TestCase(String today, String[] terms, String[] privacies) {
+        this.today = today;
+        this.terms = terms;
+        this.privacies = privacies;
+    }
+}
+
+
+class Solution {
+    public int[] solution(String today, String[] terms, String[] privacies) {
+        ArrayList<Integer> removeList = new ArrayList<Integer>();
+        
+        // Convert today to int days.
+        int intToday = convertToDays(today);
+        
+        // Create HashMap for store terms name and months.
+        Map<String, Integer> termsMap = new HashMap<String, Integer>();
+        for (String term : terms) {
+            String name = term.split(" ")[0];
+            int month = Integer.parseInt(term.split(" ")[1]);
+            termsMap.put(name, month);
+        }
+        
+        // Find terms to remove.
+        for (int i = 0; i < privacies.length; i++) {
+            int date = convertToDays(privacies[i].split(" ")[0]);
+            String term = privacies[i].split(" ")[1];
+            
+            int validDate = date + (termsMap.get(term) * 28) - 1;
+            
+            if (intToday > validDate) {
+                removeList.add(i + 1);
+            }
+        }
+        
+        int[] answer = removeList.stream().mapToInt(i -> i).toArray();
+
+        return answer;
+    }
+    
+    // Function for convert `yyyy.mm.dd` format to int days.
+    private static int convertToDays(String date) {
+        String[] dateList = date.split("\\.");
+        
+        int year = Integer.parseInt(dateList[0]);
+        int month = Integer.parseInt(dateList[1]);
+        int day = Integer.parseInt(dateList[2]);
+        
+        int days = day + (month * 28) + (year * 12 * 28);
+        
+        return days;
+    }
+}
