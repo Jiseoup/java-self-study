f0fcbf873dc03d92505ecd6e8665c0d3181853d8
Jiseoup <jseoup@gmail.com>
2025-11-04 16:00:43 +0900

    Review some highscorekit problems

--- CodingTest/HighScoreKit/ExhaustiveSearch/Rectangle.java
+++ CodingTest/HighScoreKit/ExhaustiveSearch/Rectangle.java
@@ -8,10 +8,10 @@ public class Rectangle {
     public static void main(String[] args) {
         // Examples
         List<TestCase> testCases = List.of(
-            new TestCase(new int[][] {{60, 50}, {30, 70}, {60, 30}, {80, 40}}),
-            new TestCase(new int[][] {{10, 7}, {12, 3}, {8, 15}, {14, 7}, {5, 15}}),
-            new TestCase(new int[][] {{14, 4}, {19, 6}, {6, 16}, {18, 7}, {7, 11}})
-        );
+                new TestCase(new int[][] { { 60, 50 }, { 30, 70 }, { 60, 30 }, { 80, 40 } }),
+                new TestCase(new int[][] { { 10, 7 }, { 12, 3 }, { 8, 15 }, { 14, 7 }, { 5, 15 } }),
+                new TestCase(new int[][] { { 14, 4 }, { 19, 6 }, { 6, 16 }, { 18, 7 }, { 7, 11 } }),
+                new TestCase(new int[][] { { 50, 50 }, { 50, 50 }, { 50, 50 }, { 50, 50 }, { 50, 50 } }));
 
         // Solutions
         Solution sol = new Solution();
@@ -22,7 +22,6 @@ public class Rectangle {
     }
 }
 
-
 // Create TestCase
 class TestCase {
     int[][] sizes;
@@ -32,25 +31,39 @@ class TestCase {
     }
 }
 
-
+// Solution 1. (Review)
 class Solution {
     public int solution(int[][] sizes) {
         int maxWidth = 0;
         int maxHeight = 0;
 
         for (int[] size : sizes) {
-            int width = size[0];
-            int height = size[1];
-            
-            if (width > height) {
-                maxWidth = Math.max(maxWidth, width);
-                maxHeight = Math.max(maxHeight, height);
-            }
-            else {
-                maxWidth = Math.max(maxWidth, height);
-                maxHeight = Math.max(maxHeight, width);
-            }
+            maxWidth = Math.max(maxWidth, Math.max(size[0], size[1]));
+            maxHeight = Math.max(maxHeight, Math.min(size[0], size[1]));
         }
+
         return maxWidth * maxHeight;
     }
 }
+
+// // Solution 2.
+// class Solution {
+//     public int solution(int[][] sizes) {
+//         int maxWidth = 0;
+//         int maxHeight = 0;
+
+//         for (int[] size : sizes) {
+//             int width = size[0];
+//             int height = size[1];
+
+//             if (width > height) {
+//                 maxWidth = Math.max(maxWidth, width);
+//                 maxHeight = Math.max(maxHeight, height);
+//             } else {
+//                 maxWidth = Math.max(maxWidth, height);
+//                 maxHeight = Math.max(maxHeight, width);
+//             }
+//         }
+//         return maxWidth * maxHeight;
+//     }
+// }
--- CodingTest/HighScoreKit/Sort/KNumber.java
+++ CodingTest/HighScoreKit/Sort/KNumber.java
@@ -2,20 +2,15 @@
 
 package CodingTest.HighScoreKit.Sort;
 
-import java.util.List;
-import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.*;
 
 public class KNumber {
     public static void main(String[] args) {
         // Examples
         List<TestCase> testCases = List.of(
-            new TestCase(
-                new int[] {1, 5, 2, 6, 3, 7, 4},
-                new int[][] {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}}
-            )
-        );
+                new TestCase(
+                        new int[] { 1, 5, 2, 6, 3, 7, 4 },
+                        new int[][] { { 2, 5, 3 }, { 4, 4, 1 }, { 1, 7, 3 } }));
 
         // Solutions
         Solution sol = new Solution();
@@ -25,7 +20,6 @@ public class KNumber {
         }
     }
 }
-
 
 // Create TestCase
 class TestCase {
@@ -38,27 +32,47 @@ class TestCase {
     }
 }
 
-
+// Solution 1. (Review)
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int commandsLength = commands.length;
-        int[] answer = new int[commandsLength];
-        
-        for (int x = 0; x < commandsLength; x++) {
-            ArrayList<Integer> newArray = new ArrayList<Integer>();
-            
-            // Allocate commands to variable.
-            int i = commands[x][0];
-            int j = commands[x][1];
-            int k = commands[x][2];
-            
-            // Slicing array.
-            for (int y = i - 1; y < j; y++) {
-                newArray.add(array[y]);
-            }
-            Collections.sort(newArray);
-            answer[x] = newArray.get(k - 1);
+        int size = commands.length;
+        int[] answer = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            int start = commands[i][0] - 1;
+            int end = commands[i][1];
+            int index = commands[i][2] - 1;
+
+            int[] sliced = Arrays.copyOfRange(array, start, end);
+            Arrays.sort(sliced);
+            answer[i] = sliced[index];
         }
+
         return answer;
     }
 }
+
+// // Solution 2.
+// class Solution {
+//     public int[] solution(int[] array, int[][] commands) {
+//         int commandsLength = commands.length;
+//         int[] answer = new int[commandsLength];
+
+//         for (int x = 0; x < commandsLength; x++) {
+//             ArrayList<Integer> newArray = new ArrayList<Integer>();
+
+//             // Allocate commands to variable.
+//             int i = commands[x][0];
+//             int j = commands[x][1];
+//             int k = commands[x][2];
+
+//             // Slicing array.
+//             for (int y = i - 1; y < j; y++) {
+//                 newArray.add(array[y]);
+//             }
+//             Collections.sort(newArray);
+//             answer[x] = newArray.get(k - 1);
+//         }
+//         return answer;
+//     }
+// }
